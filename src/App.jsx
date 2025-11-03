9cae37d04457b8ac21d94306bb8803c23da1f8fc
bnguien <nguyennguyendangbao8@gmail.com>
2025-11-03 11:32:48 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import './App.css'
 import StoryBubble from './components/StoryList/StoryBubble'
 import AddStoryButton from './components/StoryList/AddStoryButton'
 
-function App({onAddStory}) {
+function App({ onAddStory }) {
     const [stories, setStories] = useState([]);
     const handleAddStory = (newStory) => {
         setStories([...stories, newStory]);
     };
     return (
-        <div className="app">
+        <div className="max-w-5xl mx-auto px-5 mt-2">
             <h1>Story Feature</h1>
-            <div className="story-list">
+            <div className="scrolling overflow-y-hidden flex flex-row overflow-auto w-full h-28 whitespace-nowrap gap-1 py-2">
+                <AddStoryButton onAddStory={handleAddStory} />
                 {stories.map((story, index) => (
                     <StoryBubble key={index} image={story} />
                 ))}
             </div>
-            <AddStoryButton onAddStory={handleAddStory} />
         </div>
     );
-}   
+}
 export default App
