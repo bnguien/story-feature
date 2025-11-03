c86b3b65c4d838ab4853612021074f3e0e9566bf
bnguien <nguyennguyendangbao8@gmail.com>
2025-11-04 00:00:10 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useRef } from 'react'
 import './App.css'
-import StoryBubble from './components/StoryList/StoryBubble'
-import AddStoryButton from './components/StoryList/AddStoryButton'
-import StoryViewer from './components/StoryViewer/StoryViewer'
+import StoryList from './components/StoryList/StoryList'
 function App({ onAddStory }) {
     const [stories, setStories] = useState([]);
     const handleAddStory = (newStory) => {
@@ -11,13 +9,7 @@ function App({ onAddStory }) {
     return (
         <div className="max-w-5xl mx-auto px-5 mt-2">
             <h1>Story Feature</h1>
-            <div className="scrolling overflow-y-hidden flex flex-row overflow-auto w-full h-28 whitespace-nowrap gap-1 py-2">
-                <AddStoryButton onAddStory={handleAddStory} />
-                {stories.map((story, index) => (
-                    <StoryBubble key={index} image={story} />
-                ))}
-            </div>
-            
+            <StoryList stories={stories} onAddStory={handleAddStory} />
         </div>
     );
 }
