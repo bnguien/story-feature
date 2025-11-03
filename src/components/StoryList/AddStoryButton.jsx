6a33c786d183dafbbacf5b84573440a278599036
bnguien <nguyennguyendangbao8@gmail.com>
2025-11-04 00:00:19 +0700

    Update AddStoryButton.jsx

--- src/components/StoryList/AddStoryButton.jsx
+++ src/components/StoryList/AddStoryButton.jsx
@@ -1,16 +1,10 @@
 import React, { Component, useRef } from "react";
-
 function AddStoryButton({ onAddStory }) {
-    const handleFileChange = (e) => {
-        const file = e.target.files[0]; // Chọn file từ người dùng 
+    const handleFileChange = async (e) => {
+        const file = e.target.files[0];
         if (!file) return;
-        //Đọc và chuyển file thành chuỗi Base64
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend = () => {
-            onAddStory(reader.result);
-        }
-    }
+        await onAddStory(file);
+    };
     const fileInput = useRef(null);
     const handleClick = () => {
         fileInput.current.click();
