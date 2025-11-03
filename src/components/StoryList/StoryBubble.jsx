854c500d7f7092dbd4510bdef35d0d7474af9b67
bnguien <nguyennguyendangbao8@gmail.com>
2025-11-03 14:45:35 +0700

    Update StoryBubble.jsx

--- src/components/StoryList/StoryBubble.jsx
+++ src/components/StoryList/StoryBubble.jsx
@@ -1,14 +1,32 @@
-import React, { Component, useRef } from "react";
+import React, { useState } from "react";
+import StoryViewer from "../StoryViewer/StoryViewer";
 
-function StoryBubble({ image }) {
+function StoryBubble({ image,  stories = [image] }) {
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+
+  const handleOpenViewer = () => setIsViewerOpen(true);
+  const handleCloseViewer = () => setIsViewerOpen(false);
+
   return (
-    <div className="w-16 h-16 rounded-full overflow-hidden border-2 border-blue-500">
-      <img
-        src={image}
-        alt="story"
-        className="w-full h-full object-cover object-center"
-      />
-    </div>
+    <>
+      <div
+        onClick={handleOpenViewer}
+        className="w-16 h-16 rounded-full overflow-hidden border-2 border-blue-500 cursor-pointer shrink-0"
+      >
+        <img
+          src={image}
+          alt="story"
+          className="w-full h-full object-cover object-center"
+        />
+      </div>
+      {isViewerOpen && (
+        <StoryViewer
+          stories={stories}
+          duration={3000}
+          onAllStoriesEnd={handleCloseViewer}
+        />
+      )}
+    </>
   );
 }
 
